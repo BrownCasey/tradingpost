eaed881cb914c037167eb16b1545023fb80a25da
Casey <caseycodes@gmail.com>
2014-08-31 00:36:19 -0400

    json looks like array is not array?

--- index.php
+++ index.php
@@ -5,40 +5,18 @@
 	<title>The Bazaar</title>
     </meta>
     <script src="https://www.google.com/jsapi"></script>
-    <script>
-	// come back and investigate google.visualization.arrayToDataTable()
-	google.load("visualization", "1", {packages:["table"]});
-	google.setOnLoadCallback(drawTable);
-
-	function drawTable() {
-	    var data = new google.visualization.DataTable();
-	    data.addColumn('string', 'Item');
-	    data.addColumn('number', 'Buy');
-	    data.addColumn('number', 'Sell');
-	    data.addColumn('number', 'Timestamp');
-	    data.addRows([
-		['Iron', 10, 15, 2014082500],
-		['Iron', 11, 16, 2014082506],
-		['Wood', 5, 8, 2014082500],
-		['Wood', 5, 7, 2014082506]
-	    ]);
-
-	    var table = new google.visualization.Table(document.getElementById('table_div'));
-
-	    table.draw(data, {showRowNumber: true});
-	}
-    </script>
 </head>
 <body>
 	<form action="index.php" method="get">
 	<input type="text" name="searchbox" placeholder="Search items by name">
 	<input type="submit" value="submit"/>
+	<button onClick="showData()">Click Me</button>
 	</form>
 	<div id="table_div"></div>
-	<pre>
+	<pre id="main">
 	<?php
 	require_once("includes/common.php");
-	
+	$myRow = "hello";
 	if(isset($_GET["searchbox"])){
 	$itemQuery = $_GET["searchbox"];
 // Slow. Times out.
@@ -62,6 +40,7 @@
 		ON items.id = prices.id
 		WHERE items.name LIKE '%$itemQuery%'";
 	$result = mysql_query($sql);
+	$stack = array();
 		while($row = mysql_fetch_array($result)){
 			$array = array(
 			$row[1],
@@ -69,9 +48,13 @@
 			$row[4],
 			$row[2],
 			);
-			echo json_encode($array);
-			print_r($row);
+			array_push($stack, $array);
 		}
+	echo "<br><br>";
+	$stack = (json_encode($stack));
+	echo "<div id='chartData' style='display: none;'>";
+	echo $stack;
+	echo "</div>";
 // Slow caused by infinite loop? Methinks yes . . .
 //		while($result){
 //			$row = mysql_fetch_array($result);
@@ -88,5 +71,6 @@
 	}
 	?>
 	</pre>
+<script src="script.js"></script>
 </body>
 </html>
