1213659430901ece031480e7d0d1eb875197b020
Casey <caseycodes@gmail.com>
2014-09-16 09:46:26 -0400

    add JQueryUI autocomplete

---
+++ menu.php
@@ -0,0 +1,8 @@
+<div id="menu">
+<ul>
+	<li>Copper Ore</li>
+	<li>Silk Scrap</li>
+	<li>Stick of Butter</li>
+	<li>Glob of Ectoplasm</li>
+</ul>
+</div>
---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	var data = ["honed country coat", "malign country coat", "mighty country coat", "berserker's iron dagger of accuracy", "berserker's iron dagger of air" ];
+	$("#searchbox").autocomplete({
+		source : data
+	});
+});
