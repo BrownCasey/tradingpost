2f3410bd38f22546bd0cb5b73c882f737a6176bf
Casey <caseycodes@gmail.com>
2014-09-22 00:19:46 -0400

    add links to a few example items in ul

--- menu.php
+++ menu.php
@@ -1,8 +1,8 @@
 <div id="menu">
 <ul>
-	<li>Copper Ore</li>
-	<li>Silk Scrap</li>
-	<li>Stick of Butter</li>
-	<li>Glob of Ectoplasm</li>
+	<li><a href="index.php?id=19697">Copper Ore</a></li>
+	<li><a href="index.php?id=19748">Silk Scrap</a></li>
+	<li><a href="index.php?id=12138">Stick of Butter</a></li>
+	<li><a href="index.php?id=19721">Glob of Ectoplasm</a></li>
 </ul>
 </div>
